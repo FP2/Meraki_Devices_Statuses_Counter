d9f5a4f0cd16dc7380d07f80b211c0ce959c76d3
Fernando Iriarte <fp2iri@gmail.com>
2019-07-07 19:49:39 +0200

    Update code

--- Code_API.py
+++ Code_API.py
@@ -1,75 +1,123 @@
-#!usr/bin/env
-
 from meraki import meraki
 import csv
 from datetime import datetime
+from collections import OrderedDict
+import logging
+import requests
 
-# API Key.
-KEY = '<API KEY>'
+logging.basicConfig(filename='Devices_Counter.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
+
+logging.info('Modules imported successfully')
 
 org_ids = []  # all org_ids
 org_names = []  # all org names
 online_counter_lst = []  # all orgs online devices
 offline_counter_lst = []  # all orgs offline devices
 
-# getting all organizations
-orgs = meraki.myorgaccess(KEY)
+try:
+    # API Key.
+    KEY = 'db977e503984058237f9bfbbaf91a5e67d0dbe20'
+    try:
+        # getting all organizations
+        orgs = meraki.myorgaccess(KEY)
 
-# creating 2 lists containing the ids and names of all orgs
-for org in orgs:
-    org_ids.append(org.get('id'))
-    org_names.append(org.get('name'))
+        # creating 2 lists containing the ids and names of all orgs
+        for org in orgs:
+            org_ids.append(org.get('id'))
+            org_names.append(org.get('name'))
 
-# Creating dictionary
-org_names_ids = {names: ids for names, ids in zip(org_names, org_ids)}
+    except meraki.OrgPermissionError:
+        error_text = 'No permission to access organization.'
+        logging.error('{}'.format(error_text))
+        print('{}'.format(error_text))
 
-# Excluding unnecessary org for this project
-org_names_ids.pop('CNB - Camera')
+    # Creating list of tuples
+    org_names_ids = sorted(list(zip(org_names, org_ids)))
 
-#Getting all devices statuses
-for name, org_id in org_names_ids.items():
-    status = meraki.get_device_statuses(KEY, org_id)
-    online_counter = 0
-    offline_counter = 0
+    # Excluding unnecessary org for this project
+    org_names_ids.__delitem__(4)
+    org_names_sorted = sorted(org_names)
+    org_names_sorted.__delitem__(4)
+    print(org_names_ids)
 
-    # Counting the quantity of online and offline devices for each org
-    for item in status:
-        if item.get('status') == 'online':
-            online_counter += 1
-        elif item.get('status') == 'offline':
-            offline_counter += 1
-    online_counter_lst.append(online_counter)
-    offline_counter_lst.append(offline_counter)
+    # Getting all devices statuses
+    for i in org_names_ids:
+        status = meraki.get_device_statuses(KEY, i[1])
+        print("Scanning {} - {}".format(i[0], i[1]))
+        online_counter = 0
+        offline_counter = 0
 
-# Creating Dicts with results
-org_device_online = {name: status for name, status in zip(org_names_ids.keys(), online_counter_lst)}
-org_device_offline = {name: status for name, status in zip(org_names_ids.keys(), offline_counter_lst)}
+        # Counting online and offline devices for each org
+        for item in status:
+            if item.get('status') == 'online':
+                online_counter += 1
+            elif item.get('status') == 'offline':
+                offline_counter += 1
 
-# Setting up date and time
-date_raw = str(datetime.utcnow())
-date_no_milli = date_raw[:20]
-date_T = date_no_milli.replace(" ", "T")
-date_T_Z = date_T.replace(".", "Z")
+        logging.info('Scanned {} successfully'.format(i[0]))
 
-# Showing results to user
-print("Online Devices:")
-print(org_device_online)
-print("\n")
-print("Offline Devices:")
-print(org_device_offline)
+        online_counter_lst.append(online_counter)
+        offline_counter_lst.append(offline_counter)
 
-# Adding last column 'tempo' with the date and time in UTC
-org_device_online['tempo'] = date_T_Z
-org_device_offline['tempo'] = date_T_Z
+    logging.info('All orgs scanned successfully')
 
-# Creating .txt for the online devices (keys = columns, values = rows)
-with open('Online Devices.txt', mode='w') as online_devices_file:
-    writer = csv.DictWriter(online_devices_file, fieldnames=org_device_online.keys())
-    writer.writeheader()
-    writer.writerow(org_device_online)
+    # Creating Dicts with results
+    org_device_online = {name: status for name, status in zip(org_names_sorted, online_counter_lst)}
+    org_device_online_sorted = OrderedDict(sorted(org_device_online.items(), key=lambda t: t[0]))
+    org_device_offline = {name: status for name, status in zip(org_names_sorted, offline_counter_lst)}
+    org_device_offline_sorted = OrderedDict(sorted(org_device_offline.items(), key=lambda t: t[0]))
 
-# Creating .txt for the offline devices (keys = columns, values = rows)
-with open('Offline Devices.txt', mode='w') as offline_devices_file:
-    writer = csv.DictWriter(offline_devices_file, fieldnames=org_device_offline.keys())
-    writer.writeheader()
-    writer.writerow(org_device_offline)
+    # Setting up date and time
+    date_raw = str(datetime.utcnow())
+    date_no_milli = date_raw[:20]
+    date_T = date_no_milli.replace(" ", "T")
+    date_T_Z = date_T.replace(".", "Z")
+
+    # Showing results to user
+    print("\n")
+    print("Online Devices:")
+    print(org_device_online_sorted)
+    print("\n")
+    print("Offline Devices:")
+    print(org_device_offline_sorted)
+
+    # Adding last column 'tempo' with the date and time in UTC
+    org_device_online_sorted['tempo'] = date_T_Z
+    org_device_offline_sorted['tempo'] = date_T_Z
+
+    # Creating .txt for the online devices (keys = columns, values = rows)
+    with open('Online Devices.txt', mode='w') as online_devices_file:
+        writer = csv.DictWriter(online_devices_file, fieldnames=org_device_online_sorted.keys())
+        writer.writeheader()
+        writer.writerow(org_device_online_sorted)
+
+    logging.info('Online Devices.txt created successfully')
+
+    # Creating .txt for the offline devices (keys = columns, values = rows)
+    with open('Offline Devices.txt', mode='w') as offline_devices_file:
+        writer = csv.DictWriter(offline_devices_file, fieldnames=org_device_offline_sorted.keys())
+        writer.writeheader()
+        writer.writerow(org_device_offline_sorted)
+
+    logging.info('Offline Devices.txt created successfully')
+
+except requests.exceptions.ConnectionError:
+    co_error = "Connection error raised."
+    logging.error('{}'.format(co_error))
+    print('{}'.format(co_error))
+
+except TimeoutError:
+    to_error = "Connection timeout error raised."
+    logging.error('{}'.format(to_error))
+    print('{}'.format(to_error))
+
+except ConnectionError:
+    con_error = '"Connection error raised."'
+    logging.error('{}'.format(con_error))
+    print('{}'.format(con_error))
+
+except Exception as unknown:
+    logging.error('{}'.format(unknown))
+    tipo = (type(unknown))
+    logging.error('{}'.format(tipo))
+    print('{}'.format(unknown))
